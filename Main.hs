2526fc3d6a5ffd257c2a97dbb6155006b4ae7724
Maria Mukhina <m.avocoder@gmail.com>
2018-11-10 19:04:58 +0300

    Add calc

--- Main.hs
+++ Main.hs
@@ -1,7 +1,16 @@
 module Main where
 main = 
        putStrLn "Rate" >>
-       putStrLn (show rate) 
+       putStrLn (show rate) >>
+       putStrLn "Deposit" >>
+       putStrLn (show deposit) >>
+       putStrLn "Years" >>
+       readLn >>=
+                 \years ->
+                  putStrLn "Final deposit" >>
+                  putStrLn (show (calc years))
+
+calc =  \years -> (deposit*years*rate) + deposit        
 rate = 0.07
 deposit = 700000
                 
