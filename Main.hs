fb6c879b6857fa410f5afd8fe0ffc8ff6fa7827a
Maria Mukhina <m.avocoder@gmail.com>
2018-11-11 21:02:15 +0300

    User configurable deposit

--- Main.hs
+++ Main.hs
@@ -1,16 +1,21 @@
 module Main where
 main = 
-       putStrLn "Rate" >>
-       putStrLn (show rate) >>
-       putStrLn "Deposit" >>
-       putStrLn (show deposit) >>
-       putStrLn "Years" >>
-       readLn >>=
-                 \years ->
-                  putStrLn "Final deposit" >>
-                  putStrLn (show (calc years))
-
-calc =  \years -> (deposit*years*rate) + deposit        
+  putStrLn "Rate" >>
+  putStrLn (show rate) >>
+  putStrLn "Deposit" >>
+  readLn >>=
+    \deposit ->                     
+      putStrLn "Years" >>
+      readLn >>=
+        \years ->
+          putStrLn "Final deposit" >>
+          putStrLn (show (calc years deposit))
+          
+calc =
+  \y -> 
+  \d ->
+    (rate*d)*y+d
+                                  
 rate = 0.07
-deposit = 700000
+
                 
